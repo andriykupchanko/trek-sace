ade7e43f627f80f0185ca5b4629e6b47b9374714
andriykupchanko <kupchankoandriy@gmail.com>
2025-01-20 02:32:33 +0200

    1

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+// filepath: /Users/work/Desktop/trek-space/frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true
+  // інші налаштування
+};
+
+module.exports = nextConfig;
