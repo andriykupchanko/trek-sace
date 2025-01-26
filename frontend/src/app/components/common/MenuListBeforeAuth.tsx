457acda5fee50dc78532a5418b1033a77de1d83f
andriykupchanko <kupchankoandriy@gmail.com>
2025-01-26 02:50:34 +0200

    fix login

--- frontend/src/app/components/common/MenuListBeforeAuth.tsx
+++ frontend/src/app/components/common/MenuListBeforeAuth.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import {  Stack } from "@chakra-ui/react";
+import { Stack, Button } from "@chakra-ui/react";
+import { CiUser } from "react-icons/ci";
 
 const MenuListBeforeAuth = () => {
   return (
     <Stack spacing={6}>
       <a href="/api/auth/login">
-        Log In
+        <Button
+            variant="outline"
+            colorScheme="teal"
+            leftIcon={<CiUser />}
+            w={"100%"}
+          >
+            Log In
+          </Button>
       </a>
     </Stack>
   );
