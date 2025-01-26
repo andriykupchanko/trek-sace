1348927a91e897c5fc191db307994f9d3ca0dcdd
andriykupchanko <kupchankoandriy@gmail.com>
2025-01-26 21:10:30 +0200

    add icon

--- frontend/src/app/components/blocks/MessageCard.tsx
+++ frontend/src/app/components/blocks/MessageCard.tsx
@@ -14,15 +14,8 @@ const MessageCard: React.FC<MessageCardProps> = ({ title, description, dateMsg, 
   const formattedTime = new Date(`1970-01-01T${timeMsg}Z`).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
 
   return (
-    <div className="bg-slate-500 justify-between p-2 rounded-md">
-    <div className="flex">
-      <h2 className="w-2/5">{title}</h2>
-      <div className="flex w-3/5">
-        <p className="ml-5 mr-2">{formattedDate}</p>
-        <p>{formattedTime}</p>
-      </div>
-    </div>
-    <p className="bg-slate-400 text-slate-300 p-2 m-2 rounded-md">{description}</p>
+    <div className="bg-[#20315C] justify-between p-2 rounded-md">
+ 
     </div>
   );
 };
